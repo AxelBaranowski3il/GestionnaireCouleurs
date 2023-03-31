eb8c138f25e8ee90cf6d398e96124847f80a4061
AxelBaranowski3il <117455133+AxelBaranowski3il@users.noreply.github.com>
2023-03-31 09:04:48 +0200

    Debut du projet

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("IHM.fxml"));
+			Scene scene = new Scene(root,500,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Gestionnaire de couleurs");
+			primaryStage.getIcons().add(new Image("file:assets/logo3il.png"));
+			primaryStage.show();
+			primaryStage.setResizable(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
