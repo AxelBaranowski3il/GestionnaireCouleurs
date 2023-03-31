792f524b9357713cb936f5f263e0c6292ad5ebe6
AxelBaranowski3il <117455133+AxelBaranowski3il@users.noreply.github.com>
2023-03-31 09:49:55 +0200

    Fin des tests de Couleur avec JUnit

--- src/application/test/TestCouleur.java
+++ src/application/test/TestCouleur.java
@@ -20,4 +20,37 @@ class TestCouleur {
         assertEquals("1", String.valueOf(c.getRed()));
     }
 	
+	@Test
+    public void testGetGreen() {
+		Couleur c = new Couleur(1, 2, 3);
+        assertEquals("2", String.valueOf(c.getGreen()));
+    }
+	
+	@Test
+    public void testGetBlue() {
+		Couleur c = new Couleur(1, 2, 3);
+        assertEquals("3", String.valueOf(c.getBlue()));
+    }
+	
+	@Test
+    public void testSetRed() {
+		Couleur c = new Couleur(1, 2, 3);
+		c.setRed(3);
+        assertEquals("3", String.valueOf(c.getRed()));
+    }
+	
+	@Test
+    public void testSetGreen() {
+		Couleur c = new Couleur(1, 2, 3);
+		c.setGreen(7);
+        assertEquals("7", String.valueOf(c.getGreen()));
+    }
+	
+	@Test
+    public void testSetBlue() {
+		Couleur c = new Couleur(1, 2, 3);
+		c.setBlue(240);
+        assertEquals("240", String.valueOf(c.getBlue()));
+    }
+	
 }
